0f1ed7668b911a00839fe4955dc5ce11108ed276
jama <jama@wikischool.org>
2020-05-03 18:08:43 +0200

    better wtf struct output, still not showing all values

--- demo.js
+++ demo.js
@@ -1,5 +1,7 @@
 /* globals marked, unfetch, ES6Promise, Promise */ // eslint-disable-line no-redeclare
 
+let doc = {};
+
 if (!window.Promise) {
   window.Promise = ES6Promise;
 }
@@ -244,12 +246,17 @@ function handleInputChange() {
 }
 
 function handleOutputChange() {
+  //console.log( $outputTypeElem.value );
   $activeOutputElem = handleChange($panes, $outputTypeElem.value);
   updateLink();
 }
 
 function handleChange(panes, visiblePane) {
+
+  //console.log( panes, visiblePane );
+
   var active = null;
+
   for (var i = 0; i < panes.length; i++) {
     if (panes[i].id === visiblePane) {
       panes[i].style.display = '';
@@ -258,7 +265,9 @@ function handleChange(panes, visiblePane) {
       panes[i].style.display = 'none';
     }
   }
+
   return active;
+
 }
 
 function addCommitVersion(value, text, commit) {
@@ -357,7 +366,9 @@ function setScrollPercent(percent) {
 }
 
 function updateLink() {
+
   var outputType = '';
+
   if ($outputTypeElem.value !== 'preview') {
     outputType = 'outputType=' + $outputTypeElem.value + '&';
   }
@@ -365,6 +376,7 @@ function updateLink() {
   $permalinkElem.href = '?' + outputType + 'text=' + encodeURIComponent($markdownElem.value)
       + '&options=' + encodeURIComponent($optionsElem.value)
       + '&version=' + encodeURIComponent($markedVerElem.value);
+
   history.replaceState('', document.title, $permalinkElem.href);
 }
 
@@ -467,12 +479,11 @@ function setResponseTime(ms) {
   $responseTimeElem.textContent = amount + suffix;
 }
 
+
 function setParsed(parsed, lexed) {
 
-  // PATCH
-
   let wikitext  = $markdownElem.value; // get wiki-markup
-  let doc       = wtf( wikitext ); // create wtf object
+  doc   				= wtf( wikitext ); // create wtf object
   let html      = doc.html(); // render document to HTML
 
   try {
@@ -483,12 +494,16 @@ function setParsed(parsed, lexed) {
   $htmlElem.value = doc.html();
   //$htmlElem.value = parsed;
 
-  $lexerElem.value = JSON.stringify( doc.data, null, 4 );
+	// https://github.com/Phrogz/NeatJSON
+  $lexerElem.value = neatJSON( doc.data )
+
+  //$lexerElem.value = JSON.stringify( doc.data, null, 4 )
   //$lexerElem.value = lexed;
 
 }
 
 function messageWorker(message) {
+
   if (!markedWorker || markedWorker.working) {
     if (markedWorker) {
       clearTimeout(markedWorker.timeout);
@@ -538,7 +553,9 @@ function messageWorker(message) {
     markedWorker.working = true;
     workerTimeout(0);
   }
+
   markedWorker.postMessage(message);
+
 }
 
 function workerTimeout(seconds) {
@@ -548,3 +565,141 @@ function workerTimeout(seconds) {
     workerTimeout(seconds);
   }, 1000);
 }
+
+// see: https://github.com/Phrogz/NeatJSON
+function neatJSON(value,opts){
+
+	opts = opts || {}
+	if (!('wrap'          in opts)) opts.wrap = 80;
+	if (opts.wrap==true) opts.wrap = -1;
+	if (!('indent'        in opts)) opts.indent = '  ';
+	if (!('arrayPadding'  in opts)) opts.arrayPadding  = ('padding' in opts) ? opts.padding : 0;
+	if (!('objectPadding' in opts)) opts.objectPadding = ('padding' in opts) ? opts.padding : 0;
+	if (!('beforeComma'   in opts)) opts.beforeComma   = ('aroundComma' in opts) ? opts.aroundComma : 0;
+	if (!('afterComma'    in opts)) opts.afterComma    = ('aroundComma' in opts) ? opts.aroundComma : 0;
+	if (!('beforeColon'   in opts)) opts.beforeColon   = ('aroundColon' in opts) ? opts.aroundColon : 0;
+	if (!('afterColon'    in opts)) opts.afterColon    = ('aroundColon' in opts) ? opts.aroundColon : 0;
+	if (!('beforeColon1'  in opts)) opts.beforeColon1  = ('aroundColon1' in opts) ? opts.aroundColon1 : ('beforeColon' in opts) ? opts.beforeColon : 0;
+	if (!('afterColon1'   in opts)) opts.afterColon1   = ('aroundColon1' in opts) ? opts.aroundColon1 : ('afterColon'  in opts) ? opts.afterColon  : 0;
+	if (!('beforeColonN'  in opts)) opts.beforeColonN  = ('aroundColonN' in opts) ? opts.aroundColonN : ('beforeColon' in opts) ? opts.beforeColon : 0;
+	if (!('afterColonN'   in opts)) opts.afterColonN   = ('aroundColonN' in opts) ? opts.aroundColonN : ('afterColon'  in opts) ? opts.afterColon  : 0;
+
+	var apad   = repeat(' ',opts.arrayPadding),
+	    opad   = repeat(' ',opts.objectPadding),
+	    comma  = repeat(' ',opts.beforeComma)+','+repeat(' ',opts.afterComma),
+	    colon1 = repeat(' ',opts.beforeColon1)+':'+repeat(' ',opts.afterColon1),
+	    colonN = repeat(' ',opts.beforeColonN)+':'+repeat(' ',opts.afterColonN);
+
+	var build = memoize();
+	return build(value,'');
+
+	function memoize(){
+		var memo = new Map;
+		return function(o,indent){
+			var byIndent=memo.get(o);
+			if (!byIndent) memo.set(o,byIndent={});
+			if (!byIndent[indent]) byIndent[indent] = rawBuild(o,indent);
+			return byIndent[indent];
+		}
+	}
+
+	function rawBuild(o,indent){
+
+		if (o===null || o===undefined) return indent+'null';
+
+		else{
+			if (typeof o==='number'){
+				if (o===Infinity) {
+					return indent+'9e9999';
+				}else if (o===-Infinity){
+					return indent+'-9e9999';
+				}else if (Number.isNaN(o)){
+					return indent+'"NaN"';
+				}else{
+					var isFloat = (o === +o && o !== (o|0));
+					return indent + ((isFloat && ('decimals' in opts)) ? o.toFixed(opts.decimals) : (o+''));
+				}
+			}else if (o instanceof Array){
+				if (!o.length) return indent+"[]";
+				var pieces  = o.map(function(v){ return build(v,'') });
+				var oneLine = indent+'['+apad+pieces.join(comma)+apad+']';
+				if (opts.wrap===false || oneLine.length<=opts.wrap) return oneLine;
+				if (opts.short){
+					var indent2 = indent+' '+apad;
+					pieces = o.map(function(v){ return build(v,indent2) });
+					pieces[0] = pieces[0].replace(indent2,indent+'['+apad);
+					pieces[pieces.length-1] = pieces[pieces.length-1]+apad+']';
+					return pieces.join(',\n');
+				}else{
+					var indent2 = indent+opts.indent;
+					return indent+'[\n'+o.map(function(v){ return build(v,indent2) }).join(',\n')+'\n'+(opts.indentLast?indent2:indent)+']';
+				}
+			}else if (o instanceof Object){
+				var sortedKV=[],i=0;
+				var sort = opts.sort || opts.sorted;
+				for (var k in o){
+					var kv = sortedKV[i++] = [k,o[k]];
+					if (sort===true) kv[2] = k;
+					else if (typeof sort==='function') kv[2]=sort(k,o[k],o);
+				}
+				if (!sortedKV.length) return indent+'{}';
+				if (sort) sortedKV = sortedKV.sort(function(a,b){ a=a[2]; b=b[2]; return a<b?-1:a>b?1:0 });
+				var keyvals=sortedKV.map(function(kv){ return [JSON.stringify(kv[0]), build(kv[1],'')].join(colon1) }).join(comma);
+				var oneLine = indent+"{"+opad+keyvals+opad+"}";
+				if (opts.wrap===false || oneLine.length<opts.wrap) return oneLine;
+				if (opts.short){
+					keyvals = sortedKV.map(function(kv){ return [indent+' '+opad+JSON.stringify(kv[0]), kv[1]] });
+					keyvals[0][0] = keyvals[0][0].replace(indent+' ',indent+'{');
+					if (opts.aligned){
+						var longest = 0;
+						for (var i=keyvals.length;i--;) if (keyvals[i][0].length>longest) longest = keyvals[i][0].length;
+						var padding = repeat(' ',longest);
+						for (var i=keyvals.length;i--;) keyvals[i][0] = padRight(padding,keyvals[i][0]);
+					}
+					for (var i=keyvals.length;i--;){
+						var k=keyvals[i][0], v=keyvals[i][1];
+						var indent2 = repeat(' ',(k+colonN).length);
+						var oneLine = k+colonN+build(v,'');
+						keyvals[i] = (opts.wrap===false || oneLine.length<=opts.wrap || !v || typeof v!="object") ? oneLine : (k+colonN+build(v,indent2).replace(/^\s+/,''));
+					}
+					return keyvals.join(',\n') + opad + '}';
+				}else{
+					var keyvals=sortedKV.map(function(kvs){ kvs[0] = indent+opts.indent+JSON.stringify(kvs[0]); return kvs });
+					if (opts.aligned){
+						var longest = 0;
+						for (var i=keyvals.length;i--;) if (keyvals[i][0].length>longest) longest = keyvals[i][0].length;
+						var padding = repeat(' ',longest);
+						for (var i=keyvals.length;i--;) keyvals[i][0] = padRight(padding,keyvals[i][0]);
+					}
+					var indent2 = indent+opts.indent;
+					for (var i=keyvals.length;i--;){
+						var k=keyvals[i][0], v=keyvals[i][1];
+						var oneLine = k+colonN+build(v,'');
+						keyvals[i] = (opts.wrap===false || oneLine.length<=opts.wrap || !v || typeof v!="object") ? oneLine : (k+colonN+build(v,indent2).replace(/^\s+/,''));
+					}
+					return indent+'{\n'+keyvals.join(',\n')+'\n'+(opts.indentLast?indent2:indent)+'}'
+				}
+			}else{
+				return indent+JSON.stringify(o);
+			}
+		}
+	}
+
+	function repeat(str,times){ // http://stackoverflow.com/a/17800645/405017
+		var result = '';
+		while(true){
+			if (times & 1) result += str;
+			times >>= 1;
+			if (times) str += str;
+			else break;
+		}
+		return result;
+	}
+
+	function padRight(pad, str){
+		return (str + pad).substring(0, pad.length);
+	}
+
+}
+
+
